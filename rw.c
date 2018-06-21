d1e263e99fc2facd3d78da73fafa18057dc96995
jerushachua <jerushachua@gmail.com>
2018-06-20 19:22:53 -0700

    rw

--- rw.c
+++ rw.c
@@ -15,8 +15,10 @@
 
 static resource_t data;
 static pthread_mutex_t m;
-int writers = 0;
+static pthread_cond_t readerQ;
+static pthread_cond_t writerQ;
 int readers = 0;
+int writers = 0;
 
 /*
  * Initialize the shared structures, including those used for
@@ -24,6 +26,8 @@ int readers = 0;
  */
 void initialize_readers_writer() {
     pthread_mutex_init(&m, NULL);
+    pthread_cond_init(&readerQ, NULL);
+    pthread_cond_init(&writerQ, NULL);
 
 }
 
@@ -37,16 +41,21 @@ void initialize_readers_writer() {
  */
 
 void rw_read(char *value, int len) {
-    pthread_once(&m, initialize_readers_writer);
     pthread_mutex_lock(&m);
 
-    /*
     while( !(writers == 0)){
-        pthread_cond_wait()
-        // work here
-        pthread_mutex_unlock(&m);
+        pthread_cond_wait(&readerQ, &m);
     }
-    */
+    readers++;
+    pthread_mutex_unlock(&m);
+
+    // read here
+    printf("reading! \n");
+
+    if(--readers == 0){
+        pthread_cond_signal(&writerQ);
+    }
+    pthread_mutex_unlock(&m);
 
 }
 
@@ -59,5 +68,21 @@ void rw_read(char *value, int len) {
  *      the readers are finished.
  */
 void rw_write(char *value, int len) {
-    printf("NOTHING IMPLEMENTED YET FOR rw_write\n");
+    pthread_mutex_lock(&m);
+
+    while(!(readers == 0) & (writers ==0)){
+        pthread_cond_wait(&writerQ, &m);
+    }
+    writers++;
+    pthread_mutex_unlock(&m);
+
+    // write here
+    printf("writing! \n");
+
+    pthread_mutex_lock(&m);
+    writers--;
+    pthread_cond_signal(&writerQ);
+    pthread_broadcast_signal(&readerQ);
+    pthread_mutex_unlock(&m);
+
 }
